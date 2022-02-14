463cefeea063c6a4c1dcb2c10bfd12825c6d3bb0
niculas1989 <nicolas.maranzano@libero.it>
2022-02-14 23:25:28 +0100

    added new structure for Main.vue

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,39 +3,48 @@
     <!-- Lista in cui generare TOT elementi -->
     <ul id="list">
       <!-- Lista per i FILM -->
-      <h2 class="text-center" v-if="films.length">Film:</h2>
-      <li v-for="film in films" :key="film.id">
-        <div>Title: {{ film.title }}</div>
-        <div>Original title originale: {{ film.original_title }}</div>
-        <div>
-          Original Language:
-          <span v-if="film.original_language === 'en'"
-            ><img src="../assets/img/en.png" alt="English Flag"
-          /></span>
-          <span v-else-if="film.original_language === 'it'"
-            ><img src="../assets/img/it.png" alt="Italian Flag"
-          /></span>
-          <span v-else>{{ film.original_language }}</span>
+
+      <div class="container">
+        <h2 class="text-center" v-if="films.length">Film:</h2>
+        <div class="row">
+          <li v-for="film in films" :key="film.id" class="col-4 py-4">
+            <div>Title: {{ film.title }}</div>
+            <div>Original title originale: {{ film.original_title }}</div>
+            <div>
+              Original Language:
+              <span v-if="film.original_language === 'en'"
+                ><img src="../assets/img/en.png" alt="English Flag"
+              /></span>
+              <span v-else-if="film.original_language === 'it'"
+                ><img src="../assets/img/it.png" alt="Italian Flag"
+              /></span>
+              <span v-else>{{ film.original_language }}</span>
+            </div>
+            <div>Vote Average: {{ film.vote_average }}</div>
+          </li>
         </div>
-        <div>Vote Average: {{ film.vote_average }}</div>
-      </li>
+      </div>
+
       <!-- Lista per le SERIE TV -->
+
       <h2 class="text-center" v-if="tvSeries.length">TV Series:</h2>
-      <li v-for="serie in tvSeries" :key="serie.id">
-        <div>Title: {{ serie.name }}</div>
-        <div>Original title originale: {{ serie.name }}</div>
-        <div>
-          Original Language:
-          <span v-if="serie.original_language === 'en'"
-            ><img src="../assets/img/en.png" alt="English Flag"
-          /></span>
-          <span v-else-if="serie.original_language === 'it'"
-            ><img src="../assets/img/it.png" alt="Italian Flag"
-          /></span>
-          <span v-else>{{ serie.original_language }}</span>
-        </div>
-        <div>Vote Average: {{ serie.vote_average }}</div>
-      </li>
+      <div class="row">
+        <li v-for="serie in tvSeries" :key="serie.id" class="col-4 py-4">
+          <div>Title: {{ serie.name }}</div>
+          <div>Original title originale: {{ serie.name }}</div>
+          <div>
+            Original Language:
+            <span v-if="serie.original_language === 'en'"
+              ><img src="../assets/img/en.png" alt="English Flag"
+            /></span>
+            <span v-else-if="serie.original_language === 'it'"
+              ><img src="../assets/img/it.png" alt="Italian Flag"
+            /></span>
+            <span v-else>{{ serie.original_language }}</span>
+          </div>
+          <div>Vote Average: {{ serie.vote_average }}</div>
+        </li>
+      </div>
     </ul>
   </main>
 </template>
