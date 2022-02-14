274e1209ac6ae1a49ecde7caa27ead7cc8092aec
niculas1989 <nicolas.maranzano@libero.it>
2022-02-14 15:24:40 +0100

    added filter by FILMS & TV Series

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,8 @@
     </header>
     <main>
       <ul id="list">
-        <li v-for="(film, index) in films" :key="index">
+        <h2>Film:</h2>
+        <li v-for="film in films" :key="film.id">
           <div>Title: {{ film.title }}</div>
           <div>Original title originale: {{ film.original_title }}</div>
           <div>
@@ -21,6 +22,22 @@
           </div>
           <div>Vote Average: {{ film.vote_average }}</div>
         </li>
+        <h2>TV Series:</h2>
+        <li v-for="serie in tvSeries" :key="serie.id">
+          <div>Title: {{ serie.name }}</div>
+          <div>Original title originale: {{ serie.name }}</div>
+          <div>
+            Original Language:
+            <span v-if="(original_language = 'en')"
+              ><img src="./assets/img/en.png" alt="English Flag"
+            /></span>
+            <span v-else-if="(original_language = 'it')"
+              ><img src="./assets/img/it.png" alt="Italian Flag"
+            /></span>
+            <span v-else>{{ serie.original_language }}</span>
+          </div>
+          <div>Vote Average: {{ serie.vote_average }}</div>
+        </li>
       </ul>
     </main>
   </div>
@@ -32,6 +49,7 @@ export default {
   data() {
     return {
       films: [],
+      tvSeries: [],
       api_key: "90fa42f2bc227218b6f76248ac1d9928",
       query: "",
       english: "@assets/img/en.png",
@@ -52,6 +70,12 @@ export default {
         .get("https://api.themoviedb.org/3/search/movie", config)
         .then((res) => {
           this.films = res.data.results;
+        });
+
+      axios
+        .get("https://api.themoviedb.org/3/search/tv", config)
+        .then((res) => {
+          this.tvSeries = res.data.results;
         });
     },
   },
---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <input type="text" v-model="query" />
+    <button @click="$emit('get-api', query)">CLICCAMI</button>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: ["query"],
+  methods: {
+    newValue(changedValue) {
+      this.query = changedValue;
+      this.$emit(this.newValue, changedValue);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
