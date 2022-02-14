ff9700f691f0a72456d7b32dec95169cfccb75ad
niculas1989 <nicolas.maranzano@libero.it>
2022-02-14 16:08:38 +0100

    refactoring: created Main.vue

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,18 @@
 <template>
   <div id="general-wrapper">
     <Header @get-api="getApi" />
-    <main>
-      <ul id="list">
-        <h2>Film:</h2>
-        <li v-for="film in films" :key="film.id">
-          <div>Title: {{ film.title }}</div>
-          <div>Original title originale: {{ film.original_title }}</div>
-          <div>
-            Original Language:
-            <span v-if="film.original_language === 'en'"
-              ><img src="./assets/img/en.png" alt="English Flag"
-            /></span>
-            <span v-else-if="film.original_language === 'it'"
-              ><img src="./assets/img/it.png" alt="Italian Flag"
-            /></span>
-            <span v-else>{{ film.original_language }}</span>
-          </div>
-          <div>Vote Average: {{ film.vote_average }}</div>
-        </li>
-        <h2>TV Series:</h2>
-        <li v-for="serie in tvSeries" :key="serie.id">
-          <div>Title: {{ serie.name }}</div>
-          <div>Original title originale: {{ serie.name }}</div>
-          <div>
-            Original Language:
-            <span v-if="serie.original_language === 'en'"
-              ><img src="./assets/img/en.png" alt="English Flag"
-            /></span>
-            <span v-else-if="serie.original_language === 'it'"
-              ><img src="./assets/img/it.png" alt="Italian Flag"
-            /></span>
-            <span v-else>{{ serie.original_language }}</span>
-          </div>
-          <div>Vote Average: {{ serie.vote_average }}</div>
-        </li>
-      </ul>
-    </main>
+    <Main :films="films" :tvSeries="tvSeries" />
   </div>
 </template>
 
 <script>
 import axios from "axios";
 import Header from "./components/Header.vue";
+import Main from "./components/Main.vue";
 export default {
   components: {
     Header,
+    Main,
   },
   data() {
     return {
---
+++ src/components/Main.vue
@@ -0,0 +1,48 @@
+<template>
+  <main>
+    <ul id="list">
+      <h2>Film:</h2>
+      <li v-for="film in films" :key="film.id">
+        <div>Title: {{ film.title }}</div>
+        <div>Original title originale: {{ film.original_title }}</div>
+        <div>
+          Original Language:
+          <span v-if="film.original_language === 'en'"
+            ><img src="../assets/img/en.png" alt="English Flag"
+          /></span>
+          <span v-else-if="film.original_language === 'it'"
+            ><img src="../assets/img/it.png" alt="Italian Flag"
+          /></span>
+          <span v-else>{{ film.original_language }}</span>
+        </div>
+        <div>Vote Average: {{ film.vote_average }}</div>
+      </li>
+      <h2>TV Series:</h2>
+      <li v-for="serie in tvSeries" :key="serie.id">
+        <div>Title: {{ serie.name }}</div>
+        <div>Original title originale: {{ serie.name }}</div>
+        <div>
+          Original Language:
+          <span v-if="serie.original_language === 'en'"
+            ><img src="../assets/img/en.png" alt="English Flag"
+          /></span>
+          <span v-else-if="serie.original_language === 'it'"
+            ><img src="../assets/img/it.png" alt="Italian Flag"
+          /></span>
+          <span v-else>{{ serie.original_language }}</span>
+        </div>
+        <div>Vote Average: {{ serie.vote_average }}</div>
+      </li>
+    </ul>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "Main",
+  props: ["films", "tvSeries"],
+};
+</script>
+
+<style>
+</style>
