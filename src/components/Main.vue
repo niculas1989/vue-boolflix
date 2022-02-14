eccd53f849a187b7911348ac7bc386f089bdaab9
niculas1989 <nicolas.maranzano@libero.it>
2022-02-14 23:29:51 +0100

    added new padding

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -8,19 +8,21 @@
         <h2 class="text-center" v-if="films.length">Film:</h2>
         <div class="row">
           <li v-for="film in films" :key="film.id" class="col-4 py-4">
-            <div>Title: {{ film.title }}</div>
-            <div>Original title originale: {{ film.original_title }}</div>
+            <div class="py-2">Title: {{ film.title }}</div>
+            <div class="py-2">
+              Original title originale: {{ film.original_title }}
+            </div>
             <div>
               Original Language:
-              <span v-if="film.original_language === 'en'"
+              <span class="py-2" v-if="film.original_language === 'en'"
                 ><img src="../assets/img/en.png" alt="English Flag"
               /></span>
-              <span v-else-if="film.original_language === 'it'"
+              <span class="py-2" v-else-if="film.original_language === 'it'"
                 ><img src="../assets/img/it.png" alt="Italian Flag"
               /></span>
-              <span v-else>{{ film.original_language }}</span>
+              <span class="py-2" v-else>{{ film.original_language }}</span>
             </div>
-            <div>Vote Average: {{ film.vote_average }}</div>
+            <div class="py-2">Vote Average: {{ film.vote_average }}</div>
           </li>
         </div>
       </div>
@@ -30,19 +32,19 @@
       <h2 class="text-center" v-if="tvSeries.length">TV Series:</h2>
       <div class="row">
         <li v-for="serie in tvSeries" :key="serie.id" class="col-4 py-4">
-          <div>Title: {{ serie.name }}</div>
-          <div>Original title originale: {{ serie.name }}</div>
+          <div class="py-2">Title: {{ serie.name }}</div>
+          <div class="py-2">Original title originale: {{ serie.name }}</div>
           <div>
             Original Language:
-            <span v-if="serie.original_language === 'en'"
+            <span class="py-2" v-if="serie.original_language === 'en'"
               ><img src="../assets/img/en.png" alt="English Flag"
             /></span>
-            <span v-else-if="serie.original_language === 'it'"
+            <span class="py-2" v-else-if="serie.original_language === 'it'"
               ><img src="../assets/img/it.png" alt="Italian Flag"
             /></span>
-            <span v-else>{{ serie.original_language }}</span>
+            <span class="py-2" v-else>{{ serie.original_language }}</span>
           </div>
-          <div>Vote Average: {{ serie.vote_average }}</div>
+          <div class="py-2">Vote Average: {{ serie.vote_average }}</div>
         </li>
       </div>
     </ul>
