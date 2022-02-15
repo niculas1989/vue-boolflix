5710b476c04d67a33528eac273cf20270241dcf8
niculas1989 <nicolas.maranzano@libero.it>
2022-02-15 13:18:24 +0100

    added font-awesome + other changes in Components

--- src/components/Poster.vue
+++ src/components/Poster.vue
@@ -3,10 +3,11 @@
     <li>{{ item.title || item.name }}</li>
     <li>{{ item.original_title || item.original_name }}</li>
     <li>
-      <img v-if="hasFlags" :src="flagSrc" alt="Language Flag" class="w-50" />
+      <img v-if="hasFlags" :src="flagSrc" alt="Language Flag" class="w-75" />
       <span v-else>{{ item.original_language }}</span>
     </li>
-    <li>{{ item.vote_average }}</li>
+    <li>{{ stars }}</li>
+    <i class="fa-solid fa-star"></i>
     <li>
       <figure>
         <img :src="images" :alt="item.title" class="h-100 w-100" />
@@ -33,6 +34,9 @@ export default {
     },
     hasFlags() {
       return this.flags.includes(this.item.original_language);
+    },
+    stars() {
+      return Math.ceil(this.item.vote_average / 2);
     },
   },
 };
